7864e0b39a4c3c7a371592739ce99c4a270cd0d1
Joulence <you@example.com>
2022-12-18 17:55:35 +0200

    Connected bank API

--- script.js
+++ script.js
@@ -3,30 +3,56 @@ let currencyMenu = document.getElementById('currencyMenu');
 let currencyInput = document.getElementById('currencyInput');
 let uahInput = document.getElementById('uahInput');
 
+const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+
+fetch(url)
+    .then(res => { return res.json(); })
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {           
+            let opt = document.createElement('option');
+
+            opt.value = data[i].cc;
+            opt.innerHTML = data[i].cc;
+            currencyMenu.appendChild(opt);
+
+            let currencyFlag = currencyMenu.value;
+
+            // console.log(currencyFlag);
+
+            if (currencyFlag == data[i].cc) {
+                uahInput.value = currencyInput.value * data[i].rate;
+                console.log(currencyFlag);
+            }
+
+            currencyMenu.addEventListener('change', () => {
+                currencyFlag = currencyMenu.value;
+                if (currencyFlag == data[i].cc) {
+                    uahInput.value = currencyInput.value * data[i].rate;
+                }
+            });
+
+            currencyInput.addEventListener('input', () => {
+                if (currencyInput.value < 0) {
+                    currencyInput.value = 0;
+                }
+                if (currencyFlag == data[i].cc) {
+                    uahInput.value = currencyInput.value * data[i].rate;
+                }
+            });
+
+            uahInput.addEventListener('input', () => {
+                if (uahInput.value < 0) {
+                    uahInput.value = 0;
+                }
+                if (currencyFlag == data[i].cc) {
+                    currencyInput.value = uahInput.value / data[i].rate;
+                }                
+            });
+        }
+    })
+    .catch(err => { console.errror(error) })
+
 let currenciesPrices = {
     'usd': 37.44,
     'eur': 40
 }
-
-let currencyFlag = currencyMenu.value;
-
-uahInput.value = currencyInput.value * currenciesPrices.usd;
-
-currencyMenu.addEventListener('change', () => {
-    currencyFlag = currencyMenu.value;
-    uahInput.value = currencyInput.value * currenciesPrices[currencyFlag];
-});
-
-currencyInput.addEventListener('input', () => {
-    if (currencyInput.value < 0) {
-        currencyInput.value = 0;
-    }
-    uahInput.value = currencyInput.value * currenciesPrices[currencyFlag];
-});
-
-uahInput.addEventListener('input', () => {
-    if (uahInput.value < 0) {
-        uahInput.value = 0;
-    }
-    currencyInput.value = uahInput.value / currenciesPrices[currencyFlag];
-});
