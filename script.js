580de59b3d7d97a605031a244a875bfb00f241f2
Joulence <you@example.com>
2022-12-18 22:56:43 +0200

    remade for async function

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-// Currency rates API
 const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 
-// Getting select element and inputs
 let currencyMenu = document.getElementById('currencyMenu');
 let currencyInput = document.getElementById('currencyInput');
 let uahInput = document.getElementById('uahInput');
 
-fetch(url)
-    .then(res => { return res.json(); })
-    .then(data => {
-        for (let i = 0; i < data.length; i++) {           
-            let opt = document.createElement('option');
+async function getData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  for (let i = 0; i < data.length; i++) {           
+    let opt = document.createElement('option');
 
-            opt.value = data[i].cc;
-            opt.innerHTML = data[i].cc;
-            currencyMenu.appendChild(opt);
+    opt.value = data[i].cc;
+    opt.innerHTML = data[i].cc;
+    currencyMenu.appendChild(opt);
 
-            let currencyFlag = currencyMenu.value;
+    let currencyFlag = currencyMenu.value;
 
-            if (currencyFlag == data[i].cc) {
-                uahInput.value = currencyInput.value * data[i].rate;
-            }
+    if (currencyFlag == data[i].cc) {
+        uahInput.value = currencyInput.value * data[i].rate;
+    }
 
-            currencyMenu.addEventListener('change', () => {
-                currencyFlag = currencyMenu.value;
-                if (currencyFlag == data[i].cc) {
-                    uahInput.value = currencyInput.value * data[i].rate;
-                }
-            });
-
-            currencyInput.addEventListener('input', () => {
-                if (currencyInput.value < 0) {
-                    currencyInput.value = 0;
-                }
-                if (currencyFlag == data[i].cc) {
-                    uahInput.value = currencyInput.value * data[i].rate;
-                }
-            });
-
-            uahInput.addEventListener('input', () => {
-                if (uahInput.value < 0) {
-                    uahInput.value = 0;
-                }
-                if (currencyFlag == data[i].cc) {
-                    currencyInput.value = uahInput.value / data[i].rate;
-                }                
-            });
+    currencyMenu.addEventListener('change', () => {
+        currencyFlag = currencyMenu.value;
+        if (currencyFlag == data[i].cc) {
+            uahInput.value = currencyInput.value * data[i].rate;
         }
-    })
-    .catch(err => { console.errror(err) })
+    });
+
+    currencyInput.addEventListener('input', () => {
+        if (currencyInput.value < 0) {
+            currencyInput.value = 0;
+        }
+        if (currencyFlag == data[i].cc) {
+            uahInput.value = currencyInput.value * data[i].rate;
+        }
+    });
+
+    uahInput.addEventListener('input', () => {
+        if (uahInput.value < 0) {
+            uahInput.value = 0;
+        }
+        if (currencyFlag == data[i].cc) {
+            currencyInput.value = uahInput.value / data[i].rate;
+        }                
+    });
+}
+}
+
+getData();
